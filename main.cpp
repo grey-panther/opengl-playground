10e62a1d005b603b31ba8a93eb1ab66bd3cbcce0
Sergey Yablokov <sergapple.i@gmail.com>
2023-01-04 14:42:46 +0100

    Add basic OpenGL drawing and draw a triangle.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ static constexpr int WINDOW_WIDTH = 1024;
 static constexpr int WINDOW_HEIGHT = 728;
 static constexpr auto WINDOW_TITLE = "LearnOpenGL";
 
+static GLuint _shaderProgramId = 0;
+
 
 void onGlfwError(int error, const char* description)
 {
@@ -34,25 +36,116 @@ void processInput(GLFWwindow* window)
 }
 
 
+void loadShaderProgram()
+{
+	// Load and compile vertex shader.
+	const char* vertexShaderText =
+			"#version 330 core\n"
+			"layout (location = 0) in vec3 aPos;\n"
+			"void main() {\n"
+			"    gl_Position = vec4(aPos, 1.0);\n"
+			"}";
+
+	const GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderText, nullptr);
+	glCompileShader(vertexShader);
+
+	int vshStatus = 0;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vshStatus);
+	if (vshStatus == GL_FALSE) {
+		std::string infoLog;
+		infoLog.resize(512);
+		glGetShaderInfoLog(vertexShader, (GLint) infoLog.size(), nullptr, infoLog.data());
+		std::cerr << "Vertex shader compilation failed:\n\t" << infoLog << std::endl;
+		return;
+	}
+
+
+	// Load and compile fragment shader.
+	const char* fragmentShaderText =
+			"#version 330 core\n"
+			"out vec4 FragColor;\n"
+			"void main() {\n"
+			"    FragColor = vec4(0.99, 0.43, 0.0, 1.0);\n"
+			"}";
+
+	const GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderText, nullptr);
+	glCompileShader(fragmentShader);
+
+	int fshStatus = 0;
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fshStatus);
+	if (fshStatus == GL_FALSE) {
+		std::string infoLog;
+		infoLog.resize(512);
+		glGetShaderInfoLog(fragmentShader, (GLint) infoLog.size(), nullptr, infoLog.data());
+		std::cerr << "Fragment shader compilation failed:\n\t" << infoLog << std::endl;
+		return;
+	}
+
+
+	// Create and link shader program.
+	const GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	int programStatus = 0;
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &programStatus);
+	if (programStatus == GL_FALSE) {
+		std::string infoLog;
+		infoLog.resize(512);
+		glGetProgramInfoLog(shaderProgram, (GLint) infoLog.size(), nullptr, infoLog.data());
+		std::cerr << "Shader program link failed:\n\t" << infoLog << std::endl;
+		return;
+	}
+
+	// When the shader program is linked, shaders are no longer needed.
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	std::cout << "Shaders are successfully loaded." << std::endl;
+
+	_shaderProgramId = shaderProgram;
+}
+
+
 void doMainUpdate()
 {
 	// Clear the frame buffer.
 	glClearColor(0.1f, 0.05f, 0.05f, 1.f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	// Create vertex buffer object (vbo).
+	// Create and bind vertex array object (vao).
+	GLuint vaoId = 0;
+	glGenVertexArrays(1, &vaoId);
+	glBindVertexArray(vaoId);
+
+	// Create and bind vertex buffer object (vbo).
 	GLuint vboId = 0;
 	glGenBuffers(1, &vboId);
 	glBindBuffer(GL_ARRAY_BUFFER, vboId);
 
 	// Fill vbo.
-	const auto coords = std::array<float, 9> {
+	const int verticesAmount = 3;
+	const auto coords = std::array<float, verticesAmount * 3> {
 			-0.5f, -0.5f, 0.f,
 			0.5f, -0.5f, 0.f,
 			0.f, 0.5f, 0.f
 	};
 	const size_t bufferBytesSize = sizeof(coords[0]) * coords.size();
 	glBufferData(GL_ARRAY_BUFFER, bufferBytesSize, coords.data(), GL_STATIC_DRAW);
+
+	// Configure vertex attributes.
+	const int attrLocation = 0;
+	const int attrSize = 3;								// 3 floats of vec3.
+	const int attrStride = attrSize * sizeof(float);	// Next vertex attribute can be found after that amount of bytes.
+	glVertexAttribPointer(attrLocation, attrSize, GL_FLOAT, GL_FALSE, attrStride, nullptr);
+	glEnableVertexAttribArray(attrLocation);
+
+	glUseProgram(_shaderProgramId);
+	glDrawArrays(GL_TRIANGLES, 0, verticesAmount);
 }
 
 
@@ -97,6 +190,8 @@ int main()
 	glfwGetFramebufferSize(window, &width, &height);
 	onFrameBufferSizeChanged(window, width, height);
 	glfwSetFramebufferSizeCallback(window, onFrameBufferSizeChanged);
+
+	loadShaderProgram();
 
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
